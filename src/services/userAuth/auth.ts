0a3dc5570ff8214988621bbebbd471be9b9c48c1
Chan-0716 <chandruas888@gmail.com>
2026-07-06 21:49:10 +0530

    api url undefined issue resolved

--- src/services/userAuth/auth.ts
+++ src/services/userAuth/auth.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+export default api;
 
 export const signUpWithEmail = async (data: any) => {
   try {
-    const response = await axios.post(`/api/user/signUp`, data);
+    const response = await api.post(`/api/user/signUp`, data);
     return response.data;
   } catch (error: any) {
     throw error.response?.data || { message: "Something went wrong" };
@@ -11,7 +15,7 @@ export const signUpWithEmail = async (data: any) => {
 
 export const signUpWithMobile = async (data: any) => {
   try {
-    const response = await axios.post(`/api/user/signup`, data);
+    const response = await api.post(`/api/user/signup`, data);
     return response.data;
   } catch (error: any) {
     throw error.response?.data || { message: "Something went wrong" };
@@ -20,7 +24,7 @@ export const signUpWithMobile = async (data: any) => {
 
 export const sendOtp = async (mobileNo: string) => {
   try {
-    const response = await axios.post(`/api/user/sendOtp`, { mobileNo });
+    const response = await api.post(`/api/user/sendOtp`, { mobileNo });
     return response.data;
   } catch (error: any) {
     throw error.response?.data || { message: "OTP sending failed" };
@@ -29,7 +33,7 @@ export const sendOtp = async (mobileNo: string) => {
 
 export const verifyOtp = async (mobileNo: string, otpCode: string) => {
   try {
-    const response = await axios.post(`/api/user/verifyOtp`, {
+    const response = await api.post(`/api/user/verifyOtp`, {
       mobileNo,
       otpCode,
     });
@@ -41,7 +45,7 @@ export const verifyOtp = async (mobileNo: string, otpCode: string) => {
 
 export const loginWithEmail = async (email: string, password: string) => {
   try {
-    const response = await axios.post(`/api/user/signIn`, { email, password });
+    const response = await api.post(`/api/user/signIn`, { email, password });
     return response.data;
   } catch (error: any) {
     throw error.response?.data || { message: "Login failed" };
@@ -50,7 +54,7 @@ export const loginWithEmail = async (email: string, password: string) => {
 
 export const loginWithMobile = async (mobileNo: string, otpCode: string) => {
   try {
-    const response = await axios.post(`/api/user/signIn`, { mobileNo, otpCode });
+    const response = await api.post(`/api/user/signIn`, { mobileNo, otpCode });
     return response.data;
   } catch (error: any) {
     throw error.response?.data || { message: "Login failed" };
@@ -62,7 +66,7 @@ export const updateUserProfile = async (
   data: any
 ) => {
   try {
-    const response = await axios.post(
+    const response = await api.post(
       `/api/user/updateUserProfile`,
       data,
       {
