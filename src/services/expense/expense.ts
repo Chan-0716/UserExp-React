d909bf2eef93cef223df739894757d55dd4c4ca0
Chan-0716 <chandruas888@gmail.com>
2026-07-09 21:54:20 +0530

    vite api url changes

--- src/services/expense/expense.ts
+++ src/services/expense/expense.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: import.meta.env.EXPENSE_VITE_API_URL,
+  baseURL: import.meta.env.VITE_EXPENSE_API_URL,
 });
-
+export default api;
 
 type ExpenseData = {
   categoryId: number;
